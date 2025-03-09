0200bc706900af0579012d9b31927e5330c08999
unknown <prakharchaturvedi9860@gmail.com>
2025-03-09 19:54:22 +0530

    feat(ui): created a form for expense handling

--- src/components/ExpenseModal/ExpenseModal.jsx
+++ src/components/ExpenseModal/ExpenseModal.jsx
@@ -1,43 +1,68 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Modal from '@mui/material/Modal';
-
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import { useState } from "react";
 
 const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 400,
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
-    boxShadow: 24,
-    p: 4,
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
+function ExpenseModal({ open, handleClose }) {
+  const [expenseData, setExpenseData] = useState([
+    { title: "", price: "", date: "" },
+  ]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setExpenseData({ ...expenseData });
+
+    console.log(e.target.title.value);
+    console.log(e.target.price.value);
+    console.log(e.target.date.value);
   };
 
-function ExpenseModal({open, handleClose}) {
-    return (
-        <div>
-          
-          <Modal
-            open={open}
-            onClose={handleClose}
-            aria-labelledby="modal-modal-title"
-            aria-describedby="modal-modal-description"
-          >
-            <Box sx={style}>
-              <Typography id="modal-modal-title" variant="h6" component="h2">
-                Text in a modal
-              </Typography>
-              <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-              </Typography>
-            </Box>
-          </Modal>
-        </div>
-      );
+  return (
+    <div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <form onSubmit={handleSubmit}>
+            <div>
+              <input required type="text" placeholder="Title" name="title" />
+              <input required type="number" placeholder="Price" name="price" />
+            </div>
+            <div>
+              <select required name="catagory" defaultValue="">
+                <option value="" disabled hidden>
+                  Select category
+                </option>
+                <option value="food">Food</option>
+                <option value="travel">Travel</option>
+                <option value="entertainment">Entertainment</option>
+              </select>
+              <input required type="date" placeholder="Date" name="date" />
+            </div>
+            <div>
+              <button type="submit">Add Expense</button>
+              <button onClick={handleClose}>Cancel</button>
+            </div>
+          </form>
+        </Box>
+      </Modal>
+    </div>
+  );
 }
 
-export default ExpenseModal
+export default ExpenseModal;
